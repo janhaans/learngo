40e847defc61732b5824f208db390260d6ef8bc6
Jan Haans <jan.haans@gmail.com>
2019-09-23 14:13:21 -0700

    Added timer to quizz

--- quizz/main.go
+++ quizz/main.go
@@ -9,10 +9,15 @@ import (
 	"log"
 	"os"
 	"strings"
+	"sync"
+	"time"
 )
 
 var dirName = flag.String("d", os.Getenv("HOME"), "Directory where csv file is placed")
 var fileName = flag.String("f", "problems.csv", "Name of csv file")
+var timer = flag.Int("t", 30, "Set timer duration")
+
+var wg sync.WaitGroup
 
 type Problem struct {
 	Question string
@@ -51,21 +56,40 @@ func main() {
 	flag.Parse()
 	problems := ReadCSV(*dirName, *fileName)
 	reader := bufio.NewReader(os.Stdin)
-	var ok, nok int
 
-	for _, problem := range problems {
-		fmt.Printf("%s = ", problem.Question)
-		answer, err := reader.ReadString('\n')
-		if err != nil {
-			log.Fatalln(err)
-		}
-		answer = strings.TrimSpace(answer)
-		if answer == problem.Answer {
-			ok++
-		} else {
-			nok++
-		}
+	fmt.Print("Press a Enter to start")
+	_, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatalln(err)
 	}
 
-	fmt.Printf("Total number of questions = %d of which you gave %d correct answers\n", ok+nok, ok)
+	var ok, nok int
+
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+		for _, problem := range problems {
+			fmt.Printf("%s = ", problem.Question)
+			answer, err := reader.ReadString('\n')
+			if err != nil {
+				log.Fatalln(err)
+			}
+			answer = strings.TrimSpace(answer)
+			if answer == problem.Answer {
+				ok++
+			} else {
+				nok++
+			}
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		time.Sleep(time.Duration(*timer) * time.Second)
+	}()
+
+	wg.Wait()
+
+	fmt.Printf("\nTotal number of questions = %d of which you gave %d correct answers\n", len(problems), ok)
 }
