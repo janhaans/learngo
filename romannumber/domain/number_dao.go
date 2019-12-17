5ba7830b53d72c128ee69e048248e660f6eae4e0
Jan Haans <jan.haans@gmail.com>
2019-12-18 00:44:38 +0100

    Removed some comments from romannumber

--- romannumber/domain/number_dao.go
+++ romannumber/domain/number_dao.go
@@ -6,23 +6,6 @@ import (
 	"github.com/janhaans/learngo/romannumber/utils"
 )
 
-/*
-var (
-	romanNumbers = map[int64]string{
-		1:  "I",
-		2:  "II",
-		3:  "III",
-		4:  "IV",
-		5:  "V",
-		6:  "VI",
-		7:  "VII",
-		8:  "VIII",
-		9:  "IX",
-		10: "X",
-	}
-)
-*/
-
 func intToRoman(num int) string {
 	values := []int{
 		1000, 900, 500, 400,
@@ -69,18 +52,4 @@ func GetRomanNumber(num int64) (*RomanNumber, *utils.ApplicationError) {
 		RomanNumber: roman,
 	}, nil
 
-	/*
-		roman, ok := romanNumbers[num]
-		if ok {
-			return &RomanNumber{
-				IntNumber:   num,
-				RomanNumber: roman,
-			}, nil
-		}
-		return nil, &utils.ApplicationError{
-			Code:        "Error1",
-			StatusCode:  http.StatusBadRequest,
-			Description: "Number is outside 1-10 range",
-		}
-	*/
 }
